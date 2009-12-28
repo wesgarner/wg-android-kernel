6ca075d5b850a6d741f996297ade8d2093ea8b84
Wes Garner <wesgarner@gmail.com>
2009-12-27 22:29:26 -0600

    added quick switch for ramhack at switchrhack.sh

---
+++ switchrhack.sh
@@ -0,0 +1,8 @@
+cp -a /home/wesgarner/src/wg-android-kernel/arch/arm/mach-msm/board-sapphire.h /home/wesgarner/src/wg-android-kernel/arch/arm/mach-msm/board-sapphire.h.bak
+cp -a /home/wesgarner/src/wg-android-kernel/arch/arm/mach-msm/board-trout.h /home/wesgarner/src/wg-android-kernel/arch/arm/mach-msm/board-trout.h.bak
+
+cp -a /home/wesgarner/src/wg-android-kernel/arch/arm/mach-msm/board-sapphire.h.other /home/wesgarner/src/wg-android-kernel/arch/arm/mach-msm/board-sapphire.h
+cp -a /home/wesgarner/src/wg-android-kernel/arch/arm/mach-msm/board-trout.h.other /home/wesgarner/src/wg-android-kernel/arch/arm/mach-msm/board-trout.h
+
+cp -a /home/wesgarner/src/wg-android-kernel/arch/arm/mach-msm/board-sapphire.h.bak /home/wesgarner/src/wg-android-kernel/arch/arm/mach-msm/board-sapphire.h.other
+cp -a /home/wesgarner/src/wg-android-kernel/arch/arm/mach-msm/board-trout.h.bak /home/wesgarner/src/wg-android-kernel/arch/arm/mach-msm/board-trout.h.other

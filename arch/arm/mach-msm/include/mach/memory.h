f3b77b4facb07e7b35fab8ab6d29e2c8772d6ea9
Wes Garner <wesgarner@gmail.com>
2010-03-01 14:52:02 -0600

    Add EBI1 support patch.
    Conflicts:
    
    	arch/arm/mach-msm/board-trout.h
    	arch/arm/mach-msm/include/mach/vmalloc.h

--- arch/arm/mach-msm/include/mach/memory.h
+++ arch/arm/mach-msm/include/mach/memory.h
@@ -17,7 +17,11 @@
 #define __ASM_ARCH_MEMORY_H
 
 /* physical offset of RAM */
+#if defined(CONFIG_MSM_AMSS_SUPPORT_256MB_EBI1)
+#define PHYS_OFFSET		UL(0x19200000)
+#else
 #define PHYS_OFFSET		UL(0x10000000)
+#endif
 
 #define HAS_ARCH_IO_REMAP_PFN_RANGE
 

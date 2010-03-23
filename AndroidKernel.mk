1d6d117e732791a0c9ae5ae4a3cbd092d9b7f50e
Wes Garner <wesgarner@gmail.com>
2010-03-22 23:07:53 -0500

    copy zimage to product out path

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -19,10 +19,11 @@ $(KERNEL_CONFIG): $(KERNEL_OUT)
 $(TARGET_PREBUILT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG) $(INSTALL_MOD_PATH)
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi-
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- INSTALL_MOD_PATH=$(INSTALL_MOD_PATH) modules_install
+	$(CP) $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
 
 kernelconfig: $(KERNEL_OUT) $(KERNEL_CONFIG)
 	env KCONFIG_NOTIMESTAMP=true \
 	     $(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- menuconfig
-	cp $(KERNEL_OUT)/.config kernel/arch/arm/configs/$(KERNEL_DEFCONFIG)
+	$(CP) $(KERNEL_OUT)/.config kernel/arch/arm/configs/$(KERNEL_DEFCONFIG)
 
 endif

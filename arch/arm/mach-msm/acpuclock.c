8f35efbe45dc20de2d2ca6fb7a844b2078b10c36
Wes Garner <wesgarner@gmail.com>
2010-03-29 17:35:29 -0500

    Revert "[ARM] msm: acpuclock: Do not ramp up when going to power collapse/wfi"
    This reverts commit 4f61e20749107b535e85b9923ec08bfb98dc30b9.

--- arch/arm/mach-msm/acpuclock.c
+++ arch/arm/mach-msm/acpuclock.c
@@ -163,15 +163,13 @@ static int pc_pll_request(unsigned id, unsigned on)
 
 unsigned long acpuclk_power_collapse(void) {
 	int ret = acpuclk_get_rate();
-	if (ret > drv_state.power_collapse_khz)
-		acpuclk_set_rate(drv_state.power_collapse_khz, 1);
+	acpuclk_set_rate(drv_state.power_collapse_khz, 1);
 	return ret * 1000;
 }
 
 unsigned long acpuclk_wait_for_irq(void) {
 	int ret = acpuclk_get_rate();
-	if (ret > drv_state.wait_for_irq_khz)
-		acpuclk_set_rate(drv_state.wait_for_irq_khz, 1);
+	acpuclk_set_rate(drv_state.wait_for_irq_khz, 1);
 	return ret * 1000;
 }
 

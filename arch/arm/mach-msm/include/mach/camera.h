21d16808e33e9149dbce42c574e5317e35fc452a
Wes Garner <wesgarner@gmail.com>
2010-03-29 19:13:40 -0500

    Revert "[ARM] msm: camera: more cleanup"
    This reverts commit 323b941980da1657ee81cbf4d80a9dc6027e303f.

--- arch/arm/mach-msm/include/mach/camera.h
+++ arch/arm/mach-msm/include/mach/camera.h
@@ -83,12 +83,9 @@ struct msm_sensor_ctrl {
 };
 
 struct msm_sync {
-	/* These two queues are accessed from a process context only.  They contain
-	 * pmem descriptors for the preview frames and the stats coming from the
-	 * camera sensor.
-	 */
-	struct hlist_head pmem_frames;
-	struct hlist_head pmem_stats;
+	/* These two queues are accessed from a process context only. */
+	struct hlist_head frame; /* most-frequently accessed */
+	struct hlist_head stats;
 
 	/* The message queue is used by the control thread to send commands
 	 * to the config thread, and also by the DSP to send messages to the
@@ -96,8 +93,6 @@ struct msm_sync {
 	 * both interrupt and process context.
 	 */
 	spinlock_t msg_event_q_lock;
-	int msg_event_q_max;
-	int msg_event_q_len;
 	struct list_head msg_event_q;
 	wait_queue_head_t msg_event_wait;
 
@@ -105,8 +100,6 @@ struct msm_sync {
 	 * in interrupt context, and by the frame thread.
 	 */
 	spinlock_t prev_frame_q_lock;
-	int prev_frame_q_max;
-	int prev_frame_q_len;
 	struct list_head prev_frame_q;
 	wait_queue_head_t prev_frame_wait;
 	int unblock_poll_frame;
@@ -126,7 +119,8 @@ struct msm_sync {
 	uint8_t opencnt;
 	void *cropinfo;
 	int  croplen;
-	uint32_t pict_pp;
+	unsigned pict_pp;
+	uint8_t pp_sync_flag;
 
 	const char *apps_id;
 
